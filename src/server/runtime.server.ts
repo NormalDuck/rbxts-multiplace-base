f867828fe3b9da17bdecf40de61ee63df49682d6
NormalDuck <1juhuang2@duck.com>
2024-08-23 21:44:11 -0700

    chore: update to latest base folder

--- src/server/runtime.server.ts
+++ src/server/runtime.server.ts
@@ -1,11 +1,11 @@
 import { Flamework } from "@flamework/core";
 
 // Flamework.addPaths("src/server/components");
-Flamework.addPaths("src/server/services");
+// Flamework.addPaths("src/server/services");
 // Flamework.addPaths("src/shared/components");
 
 // Flamework.addPaths("../common/src/server/components");
-// Flamework.addPaths("../common/src/server/services");
+Flamework.addPaths("../common/src/server/services");
 Flamework.addPaths("../common/src/server/hooks");
 // Flamework.addPaths("../common/src/shared/components");
 
